cc23e5d5d3928ce79fe4dbe2541dab213497f1f1
matiasconsiglio <macastro1@uc.cl>
2022-05-25 12:08:44 +0000

    Add function to ask for initial data of quantity of students and quantity of questions of exam.

--- run.py
+++ run.py
@@ -12,8 +12,24 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project_3')
 
-sales = SHEET.worksheet('sheet')
+def get_quantity_students_questions_data():
+    """
+    Get quantity of students and quantity of questions the exam has.
+    input from user.
+    """
+    
+    print("Please enter quantity of students and questions from exam")
+    print("Data should be two numbers, separated by commas.")
+    print("Example: 2,3")
 
-data = sales.get_all_values()
+    quantity_str = input("Enter your data here: ")
 
-print(data)
+    quantity_data = quantity_str.split(",")
+
+    return quantity_data
+
+data = get_quantity_students_questions_data()
+
+data1 = data[0]
+
+print(data1)
